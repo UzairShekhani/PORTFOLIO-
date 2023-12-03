86bbd820b455bd075b7f2baa83315cbffe70f966
uzairshekhani <uzairshekhani2017@gmail.com>
2023-12-03 22:09:50 +0500

    first commit

--- app.js
+++ app.js
@@ -1,47 +1,44 @@
 // toggle icon navbar
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
 
-menuIcon.onclick=()=>{
-    menuIcon.classList.toggle("bx-x")
-    navbar.classList.toggle("active")
-}
-
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
 
 // scroll sections
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
 
 window.onscroll = () => {
-    sections.forEach(sec =>{
-        let top =window.scrollY
-        let offset= sec.offsetTop
-        let height = sec.offsetHeight
-        let id=sec.getAttribute("id")
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
 
-        if(top>= offset&& top< offset + height){
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
 
-            navLinks.forEach(links =>{
-                links.classList.remove("active")
-                document.querySelector("header nav a[href*="+ id +"]").classList.add("active")
-            })
+      sec.classList.add("show-animate");
+    } else {
+      sec.classList.remove("show-animate");
+    }
+  });
 
-            sec.classList.add("show-animate")
-        }
-        else{
-            sec.classList.remove("show-animate")
-        }
-    })
+  // sticky header
+  let header = document.querySelector("header");
 
-    // sticky header
-    let header=document.querySelector("header")
-    
-    header.classList.toggle("sticky", window.scrollY>100)
-    // remove toggle icon and navbar when click navbar links (scroll)
-    menuIcon.classList.remove("bx-x")
-    navbar.classList.remove("active")
+  header.classList.toggle("sticky", window.scrollY > 100);
+  // remove toggle icon and navbar when click navbar links (scroll)
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
 
-
-    // animation footer on scroll
-
-}
+  // animation footer on scroll
+};
